0f4ad504c9481b796f379679510bd4aede126d33
Julian Sanker <julian@sankergroup.org>
2024-04-30 19:23:39 -0400

    again

--- tracr/compiler/lib.py
+++ tracr/compiler/lib.py
@@ -329,8 +329,8 @@ def make_apwm() -> rasp.SOp:
   tone_a_sum = rasp.Aggregate(tone_a_selector, rasp.tokens * rasp.tokens, default=0)
   tone_b_sum = rasp.Aggregate(tone_b_selector, rasp.tokens * rasp.tokens, default=0)
 
-  tone_a_rms = (tone_a_sum / (0.65 - 0.25)) ** 0.5
-  tone_b_rms = (tone_b_sum / (0.85 - 0.45)) ** 0.5
+  tone_a_rms = rasp.numerical(tone_a_sum / (0.65 - 0.25)) ** 0.5
+  tone_b_rms = rasp.numerical(tone_b_sum / (0.85 - 0.45)) ** 0.5
   tone_a_rms.named("tone_a_rms")
   tone_b_rms.named("tone_b_rms")
 
